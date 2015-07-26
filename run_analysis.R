9109ff526bbb362e68e3e13a61b29775338c5404
dkrstic <dkrstic@users.noreply.github.com>
2015-07-27 00:04:29 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -19,31 +19,28 @@ x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
 x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 
 #Merges the training and the test sets to create one data set
-#Create 'x' data set
-x_data <- rbind(x_train, x_test)
-#Create 'y' data set
-y_data <- rbind(y_train, y_test)
-#Create 'subject' data set
-subject_data <- rbind(subject_train, subject_test)
+x_ds <- rbind(x_train, x_test)
+y_ds <- rbind(y_train, y_test)
+subject_ds <- rbind(subject_train, subject_test)
 
 #Extracts only the measurements on the mean and standard deviation for each measurement
 features <- read.table("./data/UCI HAR Dataset/features.txt")
 measurements <- grep("-(mean|std)\\(\\)", features[, 2])
-x_data <- x_data[, measurements]
+x_ds <- x_ds[, measurements]
 
 #Uses descriptive activity names to name the activities in the data set
 activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-y_data[, 1] <- activities[y_data[, 1], 2]
+y_ds[, 1] <- activities[y_data[, 1], 2]
 
 #Appropriately labels the data set with descriptive variable names
-names(x_data) <- features[measurements, 2]
-names(y_data) <- "activity"
-names(subject_data) <- "subject"
+names(x_ds) <- features[measurements, 2]
+names(y_ds) <- "activity"
+names(subject_ds) <- "subject"
 
 #Bind all data in one data set
-ds <- cbind(x_data, y_data, subject_data)
+ds <- cbind(x_ds, y_ds, subject_ds)
 
 #Creates a second, independent tidy data set with the average of each variable for each activity and each subject
 library(plyr)
 tidy_data <- ddply(ds, .(subject, activity), .fun=function(x) colMeans(x[, 1:length(measurements)]))
-write.table(tidy_data, file = "tidy_data.txt", row.name=FALSE)
+write.table(tidy_data, "tidy_data.txt", row.name=FALSE)
